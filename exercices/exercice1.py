1b0d70ee24d2f45c5dfe767bd4de96b37baa8f82
Erouthen <71765433+Erouthen@users.noreply.github.com>
2021-02-28 12:28:47 +0100

    Update exercice1.py

--- exercices/exercice1.py
+++ exercices/exercice1.py
@@ -1,2 +1,10 @@
-def moyenne(...):
-    return ...
+def moyenne(liste):
+    for i in range (len(liste)):
+        if i == 0:
+            result = liste[i]
+        else:
+            result = result + liste [i]
+        
+    moyene_des_notes = result / len (liste)
+    return moyene_des_notes
+    
